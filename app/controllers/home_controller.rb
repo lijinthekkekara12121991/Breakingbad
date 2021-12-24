c9c08169ae3ca1cedf20e6d315bdb18c87d9aba6
Lijin Thekkekara <lijinj@gmail.com>
2021-12-24 02:41:05 +0100

    Search characters based on name and episode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,24 @@ class HomeController < ApplicationController
   end
 
   def create
+    @options = params[:search_type]
   end
 
   def destroy
   end
+
+  def search
+    @body = []
+    unless params[:name_search].blank?
+      @body = JSON.parse((RestClient.get 'https://www.breakingbadapi.com/api/characters', { params: { name: params[:name_search].downcase }}).body)
+    end
+    unless params[:episode_search].blank?
+      characters = (JSON.parse((RestClient.get "https://www.breakingbadapi.com/api/episodes/#{params[:episode_search]}").body)[0]['characters'])
+      characters.each do |character|
+        @body += JSON.parse((RestClient.get 'https://www.breakingbadapi.com/api/characters', { params: { name: character.downcase }}).body)
+      end
+    end
+  end
+
+  private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/create'
   get 'home/destroy'
+  get 'home/characters'
+  get 'home/search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
